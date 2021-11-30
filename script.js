4ab36334c9f71887ec5dc3cfc294eabb4f5e550f
Gabriel Felix <82856500+GabrielFelix05@users.noreply.github.com>
2021-11-30 00:43:19 -0300

    if else

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const array = []
 
 array['frutas'] = Array('Banana', 'Morango', 'Maçã')
 array['pessoas'] = Array('Gabriel', 'Thiago', 'Valeri')
+
+if(array['frutas'][1] === 'Morango'){
+    alert(array['frutas'][1])
+}
+if(array['pessoas'][1] === "Thiago"){
+    alert(array['pessoas'][1])
+}
