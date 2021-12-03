c48e9f8dd35d969013ee8d96b2c5ebf2928cc3da
Gabriel Felix <82856500+GabrielFelix05@users.noreply.github.com>
2021-12-02 22:02:15 -0300

    função

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-//Array
-
-const prod1 = {}
-
-prod1.name = "Gabriel"
-prod1.sobrenome = "Felix"
-prod1.idade = 23
-
-console.log(prod1)
+function minhaSoma(a, b){
+    return a + b
+}
+console.log(minhaSoma(10, 20))
