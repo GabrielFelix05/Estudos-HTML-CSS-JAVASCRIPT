d1b7a9be04e651a93914a6a93213b0bba4994ce5
Gabriel Felix <82856500+GabrielFelix05@users.noreply.github.com>
2021-12-09 09:40:47 -0300

    Array

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-const nome = { 
+const array = new Array("gabriel", "Thiago", "Felix", "Silva")
 
+for(i = 0; i < array.length; i ++){
+    console.log(array[i])
 }
-
-nome.nome1 = "Gabriel"
-nome.nome2 = "Thiago"
-nome.nome3 = "Silvia"
-nome.nome4 = "Sandra"
-
-const {nome1, nome2} = nome
-
-console.log(nome1, nome2)
