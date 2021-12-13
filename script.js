52da15dba41d436a2889f38c4ae64ff353e73d33
Gabriel Felix <82856500+GabrielFelix05@users.noreply.github.com>
2021-12-13 10:24:15 -0300

    Add valor

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const array = new Array("gabriel", "Thiago", "Felix", "Silva")
 
+//Inserindo mais um nome após o ultimo valor
+array.push('Jeniffer')
+//Inserindo mais um nome antes do primeito valor
+array.unshift('Aghata')
+
 for(i = 0; i < array.length; i ++){
     console.log(array[i])
 }
