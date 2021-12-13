12882d71a82403f8182c727770cb6087823d6501
Gabriel Felix <82856500+GabrielFelix05@users.noreply.github.com>
2021-12-13 11:46:29 -0300

    Arrays tridimensionais

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-var array = []
-var array1 = [1, 2, 3, 4, 5, 6, 7, 8]
-var array2 = [9, 10, 11, 12, 13, 14, 15]
+var mes = []
+var primeiraSemana = []
+var segundaSemana = []
 
-array[0] = array1
-array[1] = array2
+var semana1 = [1,2,3,4,5]
+var semana2 = [6,7,8,9,10]
 
-for(i = 0; i < array.length; i++) {
-    for(x = 0; x < array[i].length; x++) {
-        console.log(array[i][x])
+var semana3 = [11,12,13,14,15]
+var semana4 = [16,17,18,19,20]
+
+primeiraSemana = [semana1, semana2]
+segundaSemana = [semana3, semana4]
+mes = [primeiraSemana, segundaSemana]
+
+
+for(i = 0; i < mes.length; i++) {
+    for(x = 0; x < mes[i].length; x++) {
+        for(y = 0; y < mes[i][x].length; y++){
+            console.log(mes)
+        }
     }
 }
