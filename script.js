7e42fc3f04414f52c186107bb3ef19bcb62da081
Gabriel Felix <82856500+GabrielFelix05@users.noreply.github.com>
2021-12-13 10:34:36 -0300

    Deletando valor da primeira posição e da ultima

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const array = new Array("gabriel", "Thiago", "Felix", "Silva")
 
 //Inserindo mais um nome após o ultimo valor
 array.push('Jeniffer')
+
 //Inserindo mais um nome antes do primeito valor
 array.unshift('Aghata')
 
-for(i = 0; i < array.length; i ++){
-    console.log(array[i])
-}
+//Deletando o ultimo valor
+array.pop()
+
+//Deletando o primeiro valor
+array.shift()
+
+console.log(array)
