34fe95eca52d4b35eb226fb42cd02519055d9e68
Gabriel Felix <82856500+GabrielFelix05@users.noreply.github.com>
2021-12-18 15:16:56 -0300

    While

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-function soBoaNoticia(nota) {
-    if(nota >= 7) {
-        console.log('Aprovado com ', nota)
-    }
-    else {
-        console.log('Reprovado com ', nota)
-    }
+let multiplicar = 0
+while(multiplicar <= 10) {
+    let valor = 2
+    let final = valor * multiplicar
+    console.log(`${valor} x ${multiplicar} = ${final}`)
+    multiplicar++
 }
-
-function soSeForVerdade(verdade) {
-    if(verdade) {
-        console.log('Isso é verdade!!')
-    }
-    else {
-        console.log('Isso é mentira!!')
-    }
-}
-
-soSeForVerdade(null)
-soSeForVerdade(undefined)
-soSeForVerdade(NaN)
-soSeForVerdade('')
-soSeForVerdade(0)
-soSeForVerdade(1)
