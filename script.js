a7190f9ae6509ccc55190ed360ed3482845f8ec5
Gabriel Felix <82856500+GabrielFelix05@users.noreply.github.com>
2021-12-18 15:28:16 -0300

    Percorrendo array e objeto com For In

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-for(i = 0; i <= 10; i++) {
-    let final = 3 * i
-    console.log(`i = ${i} = ${final}`)
+const numeros = [1,2,3,4,5,6]
+
+for(i in numeros) {
+    console.log(`i = ${numeros[i]}`)
+}
+
+const pessoas = {
+    nome1: "Gabriel",
+    nome2: "Thiago",
+    nome3: "Alex",
+    nome4: "Anderson",
+}
+
+for(tipos in pessoas) {
+    console.log(`Pessoa: ${pessoas[tipos]}`)
 }
